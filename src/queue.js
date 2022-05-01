a1305909ace03e0d0d98b4e7ffbe7eba4adec99d
zvenrm <zvennnnnn@gmail.com>
2022-05-02 00:44:44 +0300

    feat: create all tasks

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(){
+    this.head = null
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    /* function lastElem(elem){
+      if(!elem.next){
+        return elem
+      }
+      return lastElem(elem.next)
+    } */
+    return this.head
+  }
+
+  enqueue(value) {
+    if(!this.head){
+      this.head = new ListNode(value)
+    }else{
+      let curr = this.head
+      while (curr.next) {
+          curr = curr.next
+      }
+      curr.next = new ListNode(value)
+    }
+    
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const res = this.head.value
+    const temp = this.head.next;
+    this.head = temp;
+    return res
   }
 }
 
